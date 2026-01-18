4178d3748e2913cdcbcaab9c27339bad6759a7ab
Ritesh-patra <patrasagarika654@gmail.com>
2026-01-18 09:33:15 +0530

    ok

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.render("index", { title: "Chess Game" });
-  // res.send("Hellow");
+
 });
 
 io.on("connection", function (uniquesocket) {
